c894f26c1e8ec0037cdf03bf4955fd933ba2eadb
dtymakhov <dtymakhov@gmail.com>
2022-08-11 19:24:10 +0300

    Renamed Success to Succeed and added new Fail overload

--- ManagedCode.Communication/Result.cs
+++ ManagedCode.Communication/Result.cs
@@ -20,12 +20,14 @@ public class Result : BaseResult<ErrorCode>
 
     public static implicit operator Result(Error<ErrorCode> error) => new(error);
     public static implicit operator Result(List<Error<ErrorCode>> errors) => new(errors);
+    public static implicit operator Result(Exception exception) => new(Error<ErrorCode>.FromException(exception));
 
 
-    public static Result Success() => new(true);
+    public static Result Succeed() => new(true);
     public static Result Fail() => new(false);
     public static Result Fail(Error<ErrorCode> error) => new(error);
     public static Result Fail(List<Error<ErrorCode>> errors) => new(errors);
+    public static Result Fail(Exception exception) => new(Error<ErrorCode>.FromException(exception));
 }
 
 public class Result<T> : BaseResult<T, ErrorCode>
@@ -50,14 +52,16 @@ public class Result<T> : BaseResult<T, ErrorCode>
     public static implicit operator Result<T>(T value) => new(value);
     public static implicit operator Result<T>(Error<ErrorCode> error) => new(error);
     public static implicit operator Result<T>(List<Error<ErrorCode>> errors) => new(errors);
+    public static implicit operator Result<T>(Exception exception) => new(Error<ErrorCode>.FromException(exception));
 
 
-    public static Result<T> Success(T value) => new(value);
+    public static Result<T> Succeed(T value) => new(value);
     public static Result<T> Fail() => new(false);
     public static Result<T> Fail(Error<ErrorCode> error) => new(error);
     public static Result<T> Fail(List<Error<ErrorCode>> errors) => new(errors);
+    public static Result<T> Fail(Exception exception) => new(Error<ErrorCode>.FromException(exception));
 
-    
+
     public Result<T> WithError(Error<ErrorCode> error)
     {
         if (IsSuccess)
