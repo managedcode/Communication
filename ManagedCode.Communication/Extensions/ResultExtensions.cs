c53a5a9e733479f86a9ba323b1090dee341bf931
dtymakhov <dtymakhov@gmail.com>
2022-08-11 19:48:51 +0300

    Extended ResultExtensions

--- ManagedCode.Communication/Extensions/ResultExtensions.cs
+++ ManagedCode.Communication/Extensions/ResultExtensions.cs
@@ -5,13 +5,23 @@ namespace ManagedCode.Communication.Extensions;
 
 public static class ResultExtensions
 {
-    public static Task<BaseResult<T, TErrorCode>> AsTask<T, TErrorCode>(this BaseResult<T, TErrorCode> value) where TErrorCode : Enum
+    public static Task<BaseResult<T, TErrorCode>> AsTask<T, TErrorCode>(this BaseResult<T, TErrorCode> result) where TErrorCode : Enum
     {
-        return Task.FromResult(value);
+        return Task.FromResult(result);
     }
 
-    public static Task<BaseResult<TErrorCode>> AsTask<TErrorCode>(this BaseResult<TErrorCode> value) where TErrorCode : Enum
+    public static Task<BaseResult<TErrorCode>> AsTask<TErrorCode>(this BaseResult<TErrorCode> result) where TErrorCode : Enum
     {
-        return Task.FromResult(value);
+        return Task.FromResult(result);
+    }
+
+    public static Task<Result<T>> AsTask<T>(this Result<T> result)
+    {
+        return Task.FromResult(result);
+    }
+
+    public static Task<Result> AsTask(this Result result)
+    {
+        return Task.FromResult(result);
     }
 }
