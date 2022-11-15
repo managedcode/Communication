35b776080f0ca746e2edb50115ffb1c2b2f80e63
ksemenenko <mail@ksemenenko.com>
2022-11-15 18:34:29 +0100

    AsValueTask

--- ManagedCode.Communication/Extensions/ResultExtensions.cs
+++ ManagedCode.Communication/Extensions/ResultExtensions.cs
@@ -24,4 +24,28 @@ public static class ResultExtensions
     {
         return Task.FromResult(result);
     }
+    
+    #if NET6_0_OR_GREATER
+    
+    public static ValueTask<BaseResult<T, TErrorCode>> AsValueTask<T, TErrorCode>(this BaseResult<T, TErrorCode> result) where TErrorCode : Enum
+    {
+        return ValueTask.FromResult(result);
+    }
+
+    public static ValueTask<BaseResult<TErrorCode>> AsValueTask<TErrorCode>(this BaseResult<TErrorCode> result) where TErrorCode : Enum
+    {
+        return ValueTask.FromResult(result);
+    }
+
+    public static ValueTask<Result<T>> AsValueTask<T>(this Result<T> result)
+    {
+        return ValueTask.FromResult(result);
+    }
+
+    public static ValueTask<Result> AsValueTask(this Result result)
+    {
+        return ValueTask.FromResult(result);
+    }
+    
+    #endif
 }
