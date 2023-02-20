a522b441be4a26440a11a867891ac12edbcb82f9
Konstantin Semenenko <mail@ksemenenko.com>
2023-02-20 21:32:09 +0100

    benchmark

---
+++ ManagedCode.Communication.Benchmark/CreateInstanceBenchmark.cs
@@ -0,0 +1,50 @@
+using System.Reflection;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+
+namespace ManagedCode.Communication.Benchmark;
+
+
+[SimpleJob]
+[MemoryDiagnoser]
+public class CreateInstanceFailBenchmark
+{
+    
+    [Benchmark(Baseline = true)]
+    public Result ResultFail() => Result.Fail();
+    
+    [Benchmark]
+    public Result ResultFailInt() => Result.Fail<int>();
+
+    [Benchmark]
+    public Result ResultSucceedInt() => Result.Succeed<int>(5);
+    
+    
+    [Benchmark]
+    public Result ActivatorCreateInstanceGeneric() => Activator.CreateInstance<Result>();
+    
+    [Benchmark]
+    public object? ActivatorCreateInstanceType() => Activator.CreateInstance(typeof(Result));
+    
+    [Benchmark]
+    public object? ActivatorCreateInstanceTypeInt() => Activator.CreateInstance(typeof(Result<int>));
+
+    
+    [Benchmark]
+    public object? ActivatorCreateInstanceTypeError()
+    {
+        var result = (Result)Activator.CreateInstance(typeof(Result));
+        result.Errors = new[] { Error.Create("oops") };
+        return result;
+    }
+    
+    [Benchmark]
+    public object? ActivatorCreateInstanceTypeIntError()
+    {
+        var result = (Result<int>)Activator.CreateInstance(typeof(Result<int>));
+        result.Errors = new[] { Error.Create("oops") };
+        return result;
+    }
+
+}
+
---
+++ ManagedCode.Communication.Benchmark/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using ManagedCode.Communication;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        var x1 = (Result)Activator.CreateInstance(typeof(Result));
+        x1.Errors = new[] { Error.Create("message") };
+        
+        var x2 = (Result<int>)Activator.CreateInstance(typeof(Result<int>));
+        x2.Errors = new[] { Error.Create("message") };
+        
+        
+        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args
+
+#if  DEBUG
+            ,new DebugInProcessConfig()  
+#endif
+        );
+    }
+}
