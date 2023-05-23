c95cfdc97e87b26e164cdd5eca7c0cd1e83ef72e
Konstantin Semenenko <mail@ksemenenko.com>
2023-05-23 17:52:14 +0200

    helper

--- ManagedCode.Communication.Extensions/ControllerExtensions.cs
+++ ManagedCode.Communication.Extensions/ControllerExtensions.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ManagedCode.Communication.Extensions;
@@ -7,5 +8,10 @@ public static class ControllerExtensions
     public static IActionResult ToActionResult<T>(this Result<T> result)
     {
         return result.IsSuccess ? new OkObjectResult(result.Value) : new BadRequestObjectResult(result.GetError()?.Message);
+    }
+    
+    public static Microsoft.AspNetCore.Http.IResult ToHttpResult<T>(this Result<T> result)
+    {
+        return result.IsSuccess ? Results.Ok(result.Value) : Results.BadRequest(result.GetError()?.Message);
     }
 }
