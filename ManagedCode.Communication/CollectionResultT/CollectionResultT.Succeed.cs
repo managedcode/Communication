642d8d0d14c418c7f5149988fa08a0d233596160
Konstantin Semenenko <mail@ksemenenko.com>
2024-06-27 13:18:47 +0200

    CollectionResult<T> Empty()

--- ManagedCode.Communication/CollectionResultT/CollectionResultT.Succeed.cs
+++ ManagedCode.Communication/CollectionResultT/CollectionResultT.Succeed.cs
@@ -7,22 +7,27 @@ public partial struct CollectionResult<T>
 {
     public static CollectionResult<T> Succeed(T[] value, int pageNumber, int pageSize, int totalItems)
     {
-        return new CollectionResult<T>(true, value, pageNumber, pageSize, totalItems, null, default);
+        return new CollectionResult<T>(true, value, pageNumber, pageSize, totalItems, default, default);
     }
 
     public static CollectionResult<T> Succeed(IEnumerable<T> value, int pageNumber, int pageSize, int totalItems)
     {
-        return new CollectionResult<T>(true, value, pageNumber, pageSize, totalItems, null, default);
+        return new CollectionResult<T>(true, value, pageNumber, pageSize, totalItems, default, default);
     }
 
     public static CollectionResult<T> Succeed(T[] value)
     {
-        return new CollectionResult<T>(true, value, 1, value.Length, value.Length, null, default);
+        return new CollectionResult<T>(true, value, 1, value.Length, value.Length, default, default);
     }
 
     public static CollectionResult<T> Succeed(IEnumerable<T> value)
     {
         var array = value.ToArray();
-        return new CollectionResult<T>(true, array, 1, array.Length, array.Length, null, default);
+        return new CollectionResult<T>(true, array, 1, array.Length, array.Length, default, default);
+    }
+    
+    public static CollectionResult<T> Empty()
+    {
+        return new CollectionResult<T>(true, [], 0, 0, 0, default, default);
     }
 }
