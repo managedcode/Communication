d65d8d6f654c238c99da313ee921605ec7007992
ksemenenko <mail@ksemenenko.com>
2025-11-12 10:07:41 +0100

    nuget

--- ManagedCode.Communication/Properties/AssemblyInfo.cs
+++ ManagedCode.Communication/Properties/AssemblyInfo.cs
@@ -1,3 +1,5 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("ManagedCode.Communication.Orleans")]
+[assembly: InternalsVisibleTo("ManagedCode.Communication.AspNetCore")]
+[assembly: InternalsVisibleTo("ManagedCode.Communication.Extensions")]
